0ce3d5df468d3ca855591f6b0c284ba678d76511
Matisse Loupy <bigtgamer1330@gmail.com>
2025-12-07 21:24:36 -0500

    Added env to git ignore.

--- thingy.py
+++ thingy.py
@@ -3,4 +3,5 @@ import dotenv
 dotenv.load_dotenv()
 PWord = os.getenv("pass")
 
-print(f"The password is: {PWord}")
+print(
+    f"The password is: {PWord} and this will not show on github the env file is ignored")
